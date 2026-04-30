fd5c8c44c55e2c2d12d9745a73fa37ef4021a61f
Vimal Kumar <vimalkumar.codes@gmail.com>
2026-04-30 14:46:41 +0530

    Detect collision with a wall

--- main.py
+++ main.py
@@ -27,11 +27,16 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
-    #Detect collision with food
+    #Detect collision with food.
     if snake.head.distance(food) < 15:
         print("food is full")
         food.refresh()
         scoreboard.increase_score()
+
+    #Detect Collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
 
 
 
